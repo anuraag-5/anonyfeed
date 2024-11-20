36c65414c9c28309eea472c6138cd0b4c7be3042
Anurag bhoite <anuragcr7778@gmail.com>
2024-11-20 13:07:30 +0530

    minor change

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 }
  
 export default nextConfig
